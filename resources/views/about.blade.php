7493a142909227efaf2aed5acf15a9dbf29bb143
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-09 16:31:34 +0200

    feat: create about and contact views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+</head>
+<body>
+
+    <h1> {{ $title }} </h1>
+
+    @if (count($fruits) > 0)
+
+        <h3>Frutta:</h3>
+        <ul>
+            @foreach ($fruits as $fruit)
+                <li> {{ $fruit }} </li>
+            @endforeach
+        </ul>
+
+    @else
+
+        <h3>Non ci sono Frutti</h3>
+
+    @endif
+
+    <h3>User</h3>
+    <p>
+        <strong>Nome: </strong> {{ $user['name'] }} <br>
+        <strong> Cognome: </strong> {{ $user['surname'] }} <br>
+        <strong> E-mail: </strong> {{ $user['email'] }} <br>
+        <strong> Età: </strong> {{ $user['age'] }} <br>
+        <strong> Città: </strong> {{ $user['city'] }}
+    </p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,54 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $title = 'Laravel Primi Passi';
+    $title = 'Home';
 
-    $fruits = ['mela', 'arancia', 'uva', 'fragola'];
+    $fruits = ['mela', 'arancia', 'uva'];
     // $fruits = [];
 
     $user = [
         'name' => 'Luca',
         'surname' => 'Rossi',
-        'email' => 'lica@rossi.it',
+        'email' => 'luca@rossi.it',
         'age' => 28,
         'city' => 'Roma',
     ];
 
     return view('home', compact('title', 'fruits', 'user'));
+});
+
+Route::get('/about', function () {
+
+    $title = 'About';
+
+    $fruits = ['mango', 'kiwi', 'melone'];
+    // $fruits = [];
+
+    $user = [
+        'name' => 'Marco',
+        'surname' => 'Bianchi',
+        'email' => 'marco@bianchi.it',
+        'age' => 30,
+        'city' => 'Milano',
+    ];
+
+    return view('about', compact('title', 'fruits', 'user'));
+});
+
+Route::get('/contact', function () {
+
+    $title = 'Contact';
+
+    $fruits = ['fragola', 'anguria', 'pesca',];
+    // $fruits = [];
+
+    $user = [
+        'name' => 'Sara',
+        'surname' => 'Verdi',
+        'email' => 'sara@verdi.it',
+        'age' => 33,
+        'city' => 'Napoli',
+    ];
+
+    return view('contact', compact('title', 'fruits', 'user'));
 });
